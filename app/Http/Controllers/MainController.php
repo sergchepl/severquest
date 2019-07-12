458985447db40339884512c2ae178328b3079341
Serhii Chepel <serhii@saladcreative.com>
2019-07-12 11:03:05 +0300

    remove log file

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -183,7 +183,6 @@ class MainController extends Controller
                 $user = User::find($number);
                 $task = Task::find($secondNumber);
                 $ban = Ban::where('user_id', $number)->where('task_id', $secondNumber)->first();
-                \Log::debug($ban->toArray());
                 if (is_null($ban)) {
                     $text_to_admin = "Задание <b>" . $task->name . "</b> команды <b>" . $user->name . "</b> не было заблокировано!\n";
                     break;
