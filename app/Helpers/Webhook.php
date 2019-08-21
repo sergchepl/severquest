6867374caa0b0d556decfa61e9c1551a2f2dc93f
Serhii Chepel <serhii@saladcreative.com>
2019-08-21 13:56:36 +0300

    refactoring names

--- app/Helpers/Webhook.php
+++ app/Helpers/Webhook.php
@@ -12,10 +12,10 @@ use Telegram\Bot\Exceptions\TelegramResponseException;
 
 trait Webhook
 {
-    private function sendTelegramMessage(string $text, array $buttons = [], $chat_id = null)
+    private function sendTelegramMessage(string $text, array $buttons = [], $chatId = null)
     {
         return Telegram::sendMessage([
-            'chat_id' => $chat_id ?? config('telegram.channel'),
+            'chat_id' => $chatId ?? config('telegram.channel'),
             'parse_mode' => 'HTML',
             'text' => $text,
             'reply_markup' => json_encode(
@@ -36,11 +36,11 @@ trait Webhook
         ]);
     }
 
-    private function sendAnswerCallbackQuery($query_id, $text)
+    private function sendAnswerCallbackQuery($queryId, $text)
     {
         try {
             return Telegram::answerCallbackQuery([
-                'callback_query_id' => $query_id,
+                'callback_query_id' => $queryId,
                 'text' => $text,
             ]);
         } catch (TelegramResponseException $e) {}
@@ -59,21 +59,21 @@ trait Webhook
 
     private function webhookSingleTaskKeyboard($button)
     {
-        $task = Task::find($button->task_id);
+        $task = Task::find($button->taskId);
 
         switch ($button->status) {
             case 0:
                 $task->clear();
 
-                $text_to_users = "🎲 Задание <b>" . $task->name . "</b> снова доступно для выполнения всеми командами.";
-                $text_to_admin = "✅ Задание успешно очищено!\n";
+                $textToUsers = "🎲 Задание <b>" . $task->name . "</b> снова доступно для выполнения всеми командами.";
+                $textToAdmin = "🎲 Задание успешно очищено!\n";
                 break;
             case 1:
                 $task->work();
 
-                $text_to_users = "⚠️ Задание <b>" . $task->name . "</b> выполняемое командой <b>" . $task->user->name . "</b> требует доработки. Внимательно "
+                $textToUsers = "⚠️ Задание <b>" . $task->name . "</b> выполняемое командой <b>" . $task->user->name . "</b> требует доработки. Внимательно "
                     . "проверьте требования к заданию и повторите загрузку соответствующих материалов.️";
-                $text_to_admin = "✅ Задание отправлено обратно в работу!\n";
+                $textToAdmin = "⚠️ Задание отправлено обратно в работу!\n";
                 break;
             case 3:
                 $task->done();
@@ -81,8 +81,8 @@ trait Webhook
 
                 event(new ScoreUpdate($task->user));
 
-                $text_to_users = "🎉 Задание <b>" . $task->name . "</b> успешно выполнено командой <b>" . $task->user->name . "</b>.";
-                $text_to_admin = "✅ Задание успешно выполнено!\n";
+                $textToUsers = "🎉 Задание <b>" . $task->name . "</b> успешно выполнено командой <b>" . $task->user->name . "</b>.";
+                $textToAdmin = "🎉 Задание успешно выполнено!\n";
                 break;
             case 4:
                 $ban = Ban::banTask($task->user->id, $task->id);
@@ -90,29 +90,29 @@ trait Webhook
 
                 event(new BanUpdate($ban, true));
 
-                $text_to_users = "🎲 Задание <b>" . $task->name . "</b> снова доступно для выполнения всеми командами.\n🚧 Команда <b>" . $task->user->name . "</b> провалила выполнение этого задания.";
-                $text_to_admin = "✅ Задание успешно забанено!\n";
+                $textToUsers = "🎲 Задание <b>" . $task->name . "</b> снова доступно для выполнения всеми командами.\n🚧 Команда <b>" . $task->user->name . "</b> провалила выполнение этого задания.";
+                $textToAdmin = "🚧 Задание успешно забанено!\n";
                 break;
         }
         event(new TaskUpdate($task));
 
         return [
-            'to_users' => $text_to_users,
-            'to_admin' => $text_to_admin
+            'toUsers' => $textToUsers,
+            'toAdmin' => $textToAdmin
         ];
     }
 
     private function webhookCommonTaskKeyboard($button)
     {
-        $task = Task::find($button->task_id);
-        $user = User::find($button->user_id);
+        $task = Task::find($button->taskId);
+        $user = User::find($button->userId);
 
         $user->addScore($task->score);
         event(new ScoreUpdate($user));
 
         return [
-            'to_users' => "🎉 Выполнение общего задания <b>" . $task->name . "</b> засчитано команде <b>" . $user->name . "</b>.",
-            'to_admin' => "✅ Задание успешно засчитано!\n"
+            'toUsers' => "🎉 Выполнение общего задания <b>" . $task->name . "</b> засчитано команде <b>" . $user->name . "</b>.",
+            'toAdmin' => "✅ Задание успешно засчитано!\n"
         ];
     }
 }
--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -11,8 +11,8 @@ use App\Task;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Telegram\Bot\Objects\CallbackQuery;
 use Telegram\Bot\FileUpload\InputFile;
+use Telegram\Bot\Objects\CallbackQuery;
 
 class MainController extends Controller
 {
@@ -77,17 +77,17 @@ class MainController extends Controller
 
             event(new TaskUpdate($task));
 
-            $text_to_admin = "<b>Задание №" . $task->id . " пришло на проверку!</b>\n"
+            $textToAdmin = "<b>Задание №" . $task->id . " пришло на проверку!</b>\n"
             . "Название : " . $task->name . "\n"
             . "Команда : " . $task->user->name . "\n"
             . "Сообщение от команды : " . $request->text;
-            $text_to_users = "💡 Задание <b>" . $task->name . "</b> проверяется администратором, ожидайте результат проверки.";
+            $textToUsers = "💡 Задание <b>" . $task->name . "</b> проверяется администратором, ожидайте результат проверки.";
 
-            $reply_buttons = [
-                KeyboardButton::createSingleTaskButton('Выполнить', ['task_id' => $task->id, 'status' => 3]),
-                KeyboardButton::createSingleTaskButton('В Работу', ['task_id' => $task->id, 'status' => 1]),
-                KeyboardButton::createSingleTaskButton('Очистить', ['task_id' => $task->id, 'status' => 0]),
-                KeyboardButton::createSingleTaskButton('Забанить', ['task_id' => $task->id, 'status' => 4]),
+            $replyButtons = [
+                KeyboardButton::createSingleTaskButton('Выполнить', ['taskId' => $task->id, 'status' => 3]),
+                KeyboardButton::createSingleTaskButton('В Работу', ['taskId' => $task->id, 'status' => 1]),
+                KeyboardButton::createSingleTaskButton('Очистить', ['taskId' => $task->id, 'status' => 0]),
+                KeyboardButton::createSingleTaskButton('Забанить', ['taskId' => $task->id, 'status' => 4]),
             ];
         }
         if ($task->type == 2) {
@@ -95,169 +95,54 @@ class MainController extends Controller
 
             event(new BanUpdate($ban, true));
 
-            $text_to_admin = "<b>🔥 Общее задание №" . $task->id . " пришло на проверку!</b> 🔥\n"
+            $textToAdmin = "<b>🔥 Общее задание №" . $task->id . " пришло на проверку!</b> 🔥\n"
             . "Название : " . $task->name . "\n"
             . "Команда : " . \Auth::user()->name . "\n"
             . "Сообщение от команды : " . $request->text;
-            $text_to_users = "🔥 Общее задание <b>" . $task->name . "</b> команды <b>" . \Auth::user()->name . "</b> успешно сдано и проверяется администратором, ответ будет в конце игры SeverQuest.";
+            $textToUsers = "🔥 Общее задание <b>" . $task->name . "</b> команды <b>" . \Auth::user()->name . "</b> успешно сдано и проверяется администратором, ответ будет в конце игры SeverQuest.";
 
-            $reply_buttons = [
-                KeyboardButton::createCommonTaskButton('Засчитать выполнение команде', ['task_id' => $task->id, 'user_id' => Auth::user()->id]),
+            $replyButtons = [
+                KeyboardButton::createCommonTaskButton('Засчитать выполнение команде', ['taskId' => $task->id, 'userId' => Auth::user()->id]),
             ];
         }
-        $message = $this->sendTelegramMessage($text_to_admin, $reply_buttons, '-1001308540909');
+        $message = $this->sendTelegramMessage($textToAdmin, $replyButtons, '-1001308540909');
 
         if (!is_null($photo)) {
             foreach ($photo as $ph) {
                 $photo = InputFile::createFromContents(file_get_contents($ph->getRealPath()), str_random(10) . '.' . $ph->getClientOriginalExtension());
-                $message_id = $message->message_id;
+                $messageId = $message->message_id;
 
-                $this->sendTelegramPhoto($photo, $message_id);
+                $this->sendTelegramPhoto($photo, $messageId);
             }
         }
-
-        $this->sendTelegramMessage($text_to_users);
+        $this->sendTelegramMessage($textToUsers);
 
         return response('ok', 200);
     }
 
     public function webhook(Request $request)
     {
-        $callback = new CallbackQuery($request->toArray());
+        $callback = (new CallbackQuery($request->toArray()))->callback_query;
         \Log::info($callback);
 
-        if (is_null($request->callback_query)) {
+        if (is_null($callback)) {
             \Log::info($request->toArray());
             return response('Nothing', 204);
         }
-        $callback_query_id = $callback->callback_query->id;
-        $message_id = $callback->callback_query->message->message_id;
-        $callback_data = json_decode($callback->callback_query->data);
+        $callbackId = $callback->id;
+        $messageId = $callback->message->message_id;
+        $callbackData = json_decode($callback->data);
 
-        if ($callback_data->type == 'single-task') {
-            $answer = $this->webhookSingleTaskKeyboard($callback_data->data);
+        if ($callbackData->type == 'single-task') {
+            $answer = $this->webhookSingleTaskKeyboard($callbackData->data);
         }
-        if ($callback_data->type == 'common-task') {
-            $answer = $this->webhookCommonTaskKeyboard($callback_data->data);
+        if ($callbackData->type == 'common-task') {
+            $answer = $this->webhookCommonTaskKeyboard($callbackData->data);
         }
-        $this->sendTelegramMessage($answer['to_users']);
-        $this->clearMessageReplyMarkup($message_id);
-        $this->sendAnswerCallbackQuery($callback_query_id, $answer['to_admin']);
+        $this->sendTelegramMessage($answer['toUsers']);
+        $this->clearMessageReplyMarkup($messageId);
+        $this->sendAnswerCallbackQuery($callbackId, $answer['toAdmin']);
 
         return response('ok', 200);
-
-        // if ($updates->channel_post == null || $updates->channel_post->chat->id != -1001308540909) {
-        //     return response('Nothing', 204);
-        // }
-
-        // $commandText = $updates->channel_post->text;
-        // $commandArray = explode(' ', $commandText);
-        // $command = $commandArray[0];
-        // $number = count($commandArray) > 1 ? (int) $commandArray[1] : 1;
-        // $secondNumber = count($commandArray) > 2 ? (int) $commandArray[2] : 1;
-        // $text_to_admin = "";
-
-        // $task = Task::find($number);
-        // switch ($command) {
-        //     case '/list':
-        //         $users = User::orderBy('score', 'desc')->get();
-        //         foreach ($users as $k => $user) {
-        //             switch ($k) {
-        //                 case 0:$text_to_admin .= "------------------🥇------------------\n<b>ID команды:</b> " . $user->id
-        //                     . "\n<b>Название команды:</b> " . $user->name . "\n<b>Количество баллов:</b> " . $user->score . "\n";
-        //                     break;
-        //                 case 1:$text_to_admin .= "------------------🥈------------------\n<b>ID команды:</b> " . $user->id
-        //                     . "\n<b>Название команды:</b> " . $user->name . "\n<b>Количество баллов:</b> " . $user->score . "\n";
-        //                     break;
-        //                 case 2:$text_to_admin .= "------------------🥉------------------\n<b>ID команды:</b> " . $user->id
-        //                     . "\n<b>Название команды:</b> " . $user->name . "\n<b>Количество баллов:</b> " . $user->score . "\n";
-        //                     break;
-        //                 default:$text_to_admin .= "--------------------------------------\n<b>ID команды:</b> " . $user->id
-        //                     . "\n<b>Название команды:</b> " . $user->name . "\n<b>Количество баллов:</b> " . $user->score . "\n";
-        //                     break;
-        //             }
-        //         }
-        //         $text_to_admin .= "--------------------------------------\n";
-        //         break;
-        //     case '/clear_team':
-        //         $user = User::find($number);
-
-        //         if (is_null($user)) {
-        //             $text_to_admin = "Команды <b>№" . $number . "</b> не существует!\n";
-        //             break;
-        //         }
-        //         $user->score = 0;
-        //         $user->save();
-        //         $text_to_admin = "Прогресс команды <b>" . $user->name . "</b> обнулен!\n";
-
-        //         event(new Score($user));
-        //         break;
-        //     case '/clear_ban':
-        //         $user = User::find($number);
-        //         $task = Task::find($secondNumber);
-        //         $ban = Ban::where('user_id', $number)->where('task_id', $secondNumber)->first();
-        //         if (is_null($ban)) {
-        //             $text_to_admin = "Задание <b>" . $task->name . "</b> команды <b>" . $user->name . "</b> не было заблокировано!\n";
-        //             break;
-        //         }
-        //         $ban->delete();
-
-        //         $text_to_admin = "Задание <b>" . $task->name . "</b> команды <b>" . $user->name . "</b> разбанено!\n";
-        //         $text_to_users = "🚦 Задание <b>" . $task->name . "</b> команды <b>" . $user->name . "</b> разбанено!\n";
-
-        //         event(new BanUpdate($ban, false));
-
-        //         Telegram::sendMessage([
-        //             'chat_id' => config('telegram.channel'),
-        //             'parse_mode' => 'HTML',
-        //             'text' => $text_to_users,
-        //         ]);
-        //         break;
-        //     case '/add':
-        //         $user = User::find($number);
-        //         if (is_null($user)) {
-        //             $text_to_admin = "Команды с таким ID не существует!\n";
-        //             break;
-        //         }
-        //         $score_to_save = $user->score;
-        //         $user->score = $score_to_save + $secondNumber;
-        //         $user->save();
-        //         $text_to_admin = "Команде <b>" . $user->name . "</b> успешно добавлено <b>" . $secondNumber . "</b> очков!\n";
-        //         $text_to_users = "⚡️ Команде <b>" . $user->name . "</b> добавлено <b>" . $secondNumber . "</b> очков!\n";
-
-        //         event(new Score($user));
-
-        //         Telegram::sendMessage([
-        //             'chat_id' => config('telegram.channel'),
-        //             'parse_mode' => 'HTML',
-        //             'text' => $text_to_users,
-        //         ]);
-        //         break;
-        //     case '/remove':
-        //         $user = User::find($number);
-        //         if (is_null($user)) {
-        //             $text_to_admin = "Команды с таким ID не существует!\n";
-        //             break;
-        //         }
-        //         $score_to_save = $user->score;
-        //         $user->score = $score_to_save - $secondNumber;
-        //         $user->save();
-        //         $text_to_admin = "Команде <b>" . $user->name . "</b> успешно отнято <b>" . $secondNumber . "</b> очков!\n";
-        //         $text_to_users = "⚡️ Команде <b>" . $user->name . "</b> отнято <b>" . $secondNumber . "</b> очков!\n";
-
-        //         event(new Score($user));
-
-        //         Telegram::sendMessage([
-        //             'chat_id' => config('telegram.channel'),
-        //             'parse_mode' => 'HTML',
-        //             'text' => $text_to_users,
-        //         ]);
-        //         break;
-        // }
-        // Telegram::sendMessage([
-        //     'chat_id' => '-1001308540909',
-        //     'parse_mode' => 'HTML',
-        //     'text' => $text_to_admin,
-        // ]);
     }
 }
