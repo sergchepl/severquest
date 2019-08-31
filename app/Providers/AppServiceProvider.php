df0398fba9dce5932cd62b43dc95e9ae48c2a2e7
Serhii Chepel <serhii@saladcreative.com>
2019-08-31 11:24:51 +0300

    change basic app language to RU

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Carbon\Carbon;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -14,6 +16,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        Carbon::setLocale(config('app.locale'));
     }
 
     /**
